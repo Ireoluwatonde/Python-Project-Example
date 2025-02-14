b7615ab6f35f0a17922e316e50dc9d06762a8b76
Ireoluwatonde <iretondefasanu@gmail.com>
2025-02-14 15:50:22 +0000

    Adding new comment

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 def main(msg):
     print(msg)
-
+# Adding the second comment to this file.
 main("Hello People")
 # Python-Project-Example
 #This is an example project to demonstrate the use of git on Ubuntu.
